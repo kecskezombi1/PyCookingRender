c8208c33a1c470fec269bb5b4be11eb5e48b0fac
kecskezombi1 <halaszd3@gmail.com>
2026-09-13 21:06:26 +0200

    test

--- main.py
+++ main.py
@@ -9,6 +9,5 @@ class App(tk.Tk):
         root.title("Recept simulator")
         label = tk.Label(root, text="Üdvözöljünk a Recept simulatorban", font=("Arial", 15, "bold"))
         label.pack()
-
 app = App()
 app.mainloop()
